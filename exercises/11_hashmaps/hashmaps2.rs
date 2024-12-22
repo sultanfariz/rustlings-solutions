e9c2cc4367aca567da3f3736731fb100efb7ee2b
sultanfariz <hydeblazack@gmail.com>
2024-12-22 14:03:53 +0700

    11_hashmaps2

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+
+        basket.entry(Fruit::Apple);
+        if fruit != Fruit::Apple && fruit != Fruit::Mango && fruit != Fruit::Lychee {
+            basket.insert(fruit, 100);
+        }
     }
 }
 
